130b1aeed882dcc5af2ac568136b63b242c35ba7
cdragon-1 <streetfood89@gmail.com>
2017-03-14 17:30:14 +0900

    사용자 모델에 엔드포인트 추가, 코드 조각 연결, 시리얼라이저 owner 업데이트

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,10 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -12,3 +16,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
